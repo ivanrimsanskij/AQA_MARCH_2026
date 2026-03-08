e145c0c826a212d0611d31b5170b9638d107f86e
ivanrimsanskij <ivanrimsanskij@gmail.com>
2026-03-08 17:00:12 +0100

    Create HomeworkIvan.java

---
+++ src/org/prog/session1/HomeworkIvan.java
@@ -0,0 +1,14 @@
+package org.prog.session1;
+
+public class HomeworkIvan {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+
+        if (i == j) {
+            System.out.println("OK");
+        } else {
+            System.out.println("NOT OK");
+        }
+    }
+}

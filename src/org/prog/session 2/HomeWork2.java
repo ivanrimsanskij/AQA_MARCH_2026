9f1b595e01e0696b79fec9ff3a5fa089ada4b2a2
ivanrimsanskij <ivanrimsanskij@gmail.com>
2026-03-13 23:52:12 +0100

    Did HomeWork2

---
+++ src/org/prog/session 2/HomeWork2.java
@@ -0,0 +1,36 @@
+public class HomeWork2 {
+    public static void main(String[] args) {
+        String[] emails = new String[] {
+                "abcd@gmail.com",
+                "john.doe@gmail.com",
+                "mymailgmail.com",
+                "badmail@@gmail.com",
+                "somemail@gmailcom",
+                "somemail@gmail.com",
+                "a@gmail.com",
+                "abcdedfg@x.com",
+                "abcdedfg@xzfgdasd.com"
+        };
+
+        for (int i = 0; i < emails.length; i++) {
+            String pochta = emails[i];
+
+            if (!pochta.contains("@@") && (pochta.indexOf("@") > 3)
+                    && ((pochta.lastIndexOf(".") - pochta.indexOf("@") > 3) && pochta.contains("@") && pochta.contains("."))
+                    && (pochta.split("@"))[1].contains(".")) {
+                System.out.println("valid email: " + pochta);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+

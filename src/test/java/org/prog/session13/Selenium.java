acc27138c76b9c9b35c9360ae1bcac0c24c02709
ivanrimsanskij <ivanrimsanskij@gmail.com>
2026-05-03 01:52:42 +0200

    Update Selenium.java

--- src/test/java/org/prog/session13/Selenium.java
+++ src/test/java/org/prog/session13/Selenium.java
@@ -37,6 +37,9 @@ public class Selenium {
         List<String> codes = alloPage.hasItemCode("product-sku__value", amount);
         List<String> names = alloPage.hasNameProduct("product-card__title", amount);
 
+        System.out.println("Names found: " + names.size());
+        System.out.println("Codes found: " + codes.size());
+
         DataManager.DATA.put("iphone_codes", codes);
         DataManager.DATA.put("iphone_names", names);
 
